01f8810e13cf7ae1679d28cf938324199a7df320
Joel Bartlett <joel@murbar.com>
2019-03-11 12:39:22 -0400

    Add title

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Build Week Scaffold</h1>
+        <h1>Donor Management</h1>
       </div>
     );
   }
