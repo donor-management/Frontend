6331a28f067b4529b51a4bf496c9e045ef8a63bc
Joel Bartlett <joel@murbar.com>
2019-03-11 16:52:56 -0400

    Update styles

--- src/components/common/Button.jsx
+++ src/components/common/Button.jsx
@@ -4,13 +4,18 @@ import styled from 'styled-components';
 const StyledButton = styled.button`
   background: #777;
   color: white;
-  padding: 0.25rem 0.5rem;
+  padding: 0.5rem 0.75rem;
   font-size: 0.75rem;
-  border: 2px solid #777;
+  /* border: 2px solid #777; */
+  border: none;
   border-radius: 0.25rem;
   height: 2rem;
   font-weight: bold;
   margin: 0.25rem 0;
+  cursor: pointer;
+  &:hover {
+    background: #4acaa8;
+  }
 `;
 
 const Input = ({ children }) => {
