ab30226637d20033b4416f4a7c0370470816bde7
Joel Bartlett <joel@murbar.com>
2019-03-11 13:19:47 -0400

    Stub out form

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,7 +1,16 @@
 import React from 'react';
 
 const LoginForm = props => {
-  return <main>Login form</main>;
+  return (
+    <main>
+      <h1>Log in</h1>
+      <form>
+        <input type="text" placeholder="Username" />
+        <input type="password" placeholder="Password" />
+        <button>Log in</button>
+      </form>
+    </main>
+  );
 };
 
 export default LoginForm;
