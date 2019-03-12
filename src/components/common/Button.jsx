a6f5c2776a10ff4c46a8ecfdc13189b52c981e4b
Joel Bartlett <joel@murbar.com>
2019-03-11 21:05:45 -0400

    Update styles

--- src/components/common/Button.jsx
+++ src/components/common/Button.jsx
@@ -5,16 +5,18 @@ const StyledButton = styled.button`
   background: #777;
   color: white;
   padding: 0.5rem 0.75rem;
-  font-size: 0.75rem;
+  font-size: 1rem;
   /* border: 2px solid #777; */
   border: none;
   border-radius: 0.25rem;
-  height: 2rem;
   font-weight: bold;
   margin: 0.25rem 0;
   cursor: pointer;
   &:hover {
     background: #4acaa8;
+  }
+  &:focus {
+    outline: none;
   }
 `;
 
