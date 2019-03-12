b52d28851fecadbb2880785733914971e32fb8af
Joel Bartlett <joel@murbar.com>
2019-03-11 22:00:41 -0400

    Redirect based on logged in status

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
+import auth from '../services/authService';
+import { Redirect } from 'react-router-dom';
 
 const HomePage = () => {
-  return <h1>Welcome!</h1>;
+  const user = auth.getCurrentUser();
+  if (user) return <Redirect to="/dashboard" />;
+
+  return <Redirect to="/login" />;
+
+  // return <h1>Welcome!</h1>;
 };
 
 export default HomePage;
