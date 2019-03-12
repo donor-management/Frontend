3d940a0a6393c2820c405c8c114d58c7b8b0ba83
Joel Bartlett <joel@murbar.com>
2019-03-12 13:22:06 -0400

    Add routes

--- src/App.js
+++ src/App.js
@@ -10,9 +10,13 @@ import DashboardPage from './components/DashboardPage';
 import ProtectedRoute from './components/common/ProtectedRoute';
 import Logout from './components/Logout';
 import NotFound from './components/NotFound';
+import DonorsPage from './components/DonorsPage';
+import CampaignsPage from './components/CampaignsPage';
 
 const StyledMain = styled.main`
-  /* padding: 0 1.5rem; */
+  section {
+    padding: 0 1.5rem;
+  }
 `;
 
 const App = () => {
@@ -27,6 +31,8 @@ const App = () => {
               <Route path="/login" component={LoginForm} />
               <Route path="/logout" component={Logout} />
               <ProtectedRoute path="/dashboard" component={DashboardPage} />
+              <ProtectedRoute path="/donors" component={DonorsPage} />
+              <ProtectedRoute path="/campaigns" component={CampaignsPage} />
               <Route path="/" component={HomePage} />
               <Route component={NotFound} />
             </Switch>
