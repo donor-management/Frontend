322cd46fc65607d4956406241a85de86d84ba04c
Joel Bartlett <joel@murbar.com>
2019-03-12 23:22:09 -0400

    Add hook for donor resource

---
+++ src/hooks/useDonorService.js
@@ -0,0 +1,56 @@
+import { useState, useEffect, useContext } from 'react';
+import donorService from '../services/donorService';
+import { AuthContext } from '../store/AuthContext';
+
+const useDonorService = () => {
+  const { user } = useContext(AuthContext);
+  const [donors, setDonors] = useState([]);
+
+  const getDonors = async () => {
+    const { data } = await donorService.getAll();
+    setDonors(
+      data.sort((a, b) => {
+        return a.last_contact - b.last_contact;
+      })
+    );
+  };
+
+  useEffect(() => {
+    getDonors();
+  }, []);
+
+  const saveDonor = async donor => {
+    donor.org_id = user.org_id;
+    const { data } = await donorService.save(donor);
+    setDonors(prev => [data.donor, ...prev]);
+  };
+
+  const updateDonor = async donor => {
+    const { data } = await donorService.save(donor);
+    setDonors(
+      donors.map(d => {
+        if (d.id !== data.donor.id) return d;
+        return data.donor;
+      })
+    );
+  };
+
+  const deleteDonor = async id => {
+    const { data } = await donorService.delete(id);
+    if (data === 1) {
+      setDonors(donors.filter(d => d.id !== id));
+    }
+  };
+
+  return [
+    donors,
+    {
+      getAll: getDonors,
+      save: saveDonor,
+      update: updateDonor,
+      delete: deleteDonor
+    }
+  ];
+};
+
+export default useDonorService;
