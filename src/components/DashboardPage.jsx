2c21e30da13f5ee2f609ce409083cb8cca9c9625
Joel Bartlett <joel@murbar.com>
2019-03-13 17:35:27 -0400

    Add dashboard metrics

--- src/components/DashboardPage.jsx
+++ src/components/DashboardPage.jsx
@@ -1,23 +1,78 @@
 import React, { useContext } from 'react';
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
 import { AuthContext } from '../store/AuthContext';
+import { AppDataContext } from '../store/AppDataContext';
 import capitalize from '../helpers/capitalize';
+import pluralize from '../helpers/pluralize';
 import DashNav from './DashNav';
+import isStale from '../helpers/isStale';
+import formatDollars from '../helpers/formatDollars';
+
+const StyledDashContainer = styled.section`
+  .dash-tile {
+    margin-bottom: 1rem;
+    padding: 2rem;
+    background: #5c6ac4;
+    border-radius: 0.5rem;
+    color: white;
+    font-size: 1.5rem;
+  }
+  .figure {
+    font-size: 2.5rem;
+    font-weight: bold;
+  }
+  .caption span {
+    display: block;
+    font-weight: bold;
+  }
+`;
 
 const DashboardPage = () => {
   const { user } = useContext(AuthContext);
+  const { donors, campaigns } = useContext(AppDataContext);
   const name = capitalize(user.username);
+
+  console.log(donors, campaigns);
+
+  const activeCampaignsCount = campaigns.reduce((count, campaign) => {
+    return campaign.active_campaign === 1 ? count + 1 : count;
+  }, 0);
+
+  const staleDonorsCount = donors.reduce((count, donor) => {
+    return isStale(donor.last_contacted) ? count + 1 : count;
+  }, 0);
+
+  const totalContributions = donors.reduce((total, donor) => {
+    return total + donor.total_donations;
+  }, 0);
 
   return (
     <div className="dashboard-wrapper">
       <DashNav />
-      <section>
+      <StyledDashContainer>
         <h1>Welcome, {name}</h1>
-        [$$] raised in the last 30 days
-        <p>Donors last contacted more than 60 days ago</p>
-        [table of donors]
-        <p>Active campaigns</p>
-        [table of campaigns]
-      </section>
+        {!donors.length && (
+          <p className="no-donors">
+            You have no donors. <Link to="/donors">Add a donor</Link>
+          </p>
+        )}
+        <div className="dash-tile">
+          <div className="figure">{formatDollars(totalContributions)}</div>
+          <div className="caption">collected</div>
+        </div>
+        <div className="dash-tile">
+          <div className="figure">{staleDonorsCount}</div>
+          <div className="caption">
+            donor{pluralize(staleDonorsCount)} last contacted more than 60 days ago
+            {!staleDonorsCount && <span>Good work!</span>}
+          </div>
+        </div>
+        <div className="dash-tile">
+          <div className="figure">{activeCampaignsCount}</div>
+          <div className="caption">active campaign{pluralize(activeCampaignsCount)}</div>
+        </div>
+      </StyledDashContainer>
     </div>
   );
 };
