ba581a7f82044808cfa7867532ff5dbd6e5d17ed
Joel Bartlett <joel@murbar.com>
2019-03-14 00:02:20 -0400

    Update styles

--- src/components/common/DataListContainer.jsx
+++ src/components/common/DataListContainer.jsx
@@ -4,12 +4,15 @@ import styled from 'styled-components';
 const DataListItem = styled.div`
   & > div {
     background: #f4f4f4;
-    margin-bottom: 0.5rem;
-    padding: 0.75rem;
+    margin-bottom: 0.75rem;
     border-radius: 0.5rem;
     position: relative;
+    box-shadow: 2px 2px 5px #ddd;
     &:hover {
       background: #eee;
+    }
+    &:last-child {
+      margin-bottom: 0;
     }
   }
 `;
