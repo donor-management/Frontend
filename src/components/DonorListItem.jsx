c5fbf68f72c8f659a755aaa2e9191a4984bf935c
Joel Bartlett <joel@murbar.com>
2019-03-13 21:25:50 -0400

    Style donors

--- src/components/DonorListItem.jsx
+++ src/components/DonorListItem.jsx
@@ -5,21 +5,48 @@ import getDate from '../helpers/getDate';
 import isStale from '../helpers/isStale';
 import formatDollars from '../helpers/formatDollars';
 import ActionButton from './common/ActionButton';
-import { updateLocale } from 'moment';
 
 const DonorListItemContainer = styled.div`
-  .donor-name {
-    width: 25%;
+  height: 9rem;
+  min-width: 30rem;
+  .name {
+    position: absolute;
+    top: 1rem;
+    left: 1rem;
+    font-size: 145%;
   }
-  .donor-contributions {
-    width: 15%;
+  .contact {
+    position: absolute;
+    bottom: 1rem;
+    left: 1rem;
   }
-  .donor-contact {
-    width: 25%;
-    overflow: hidden;
+  .contributions {
+    position: absolute;
+    bottom: 1rem;
+    right: 1rem;
+    text-align: right;
+    font-size: 125%;
+    font-weight: bold;
+    .label {
+      font-size: 1rem;
+      font-weight: normal;
+    }
   }
-  .donor-last-contact {
-    width: 20%;
+  .stale {
+    position: absolute;
+    top: 3.4rem;
+    left: 1rem;
+    background: #5c6ac4;
+    padding: 0.2rem 0.5rem;
+    color: white;
+    border-radius: 0.25rem;
+    font-size: 80%;
+    font-weight: bold;
+  }
+  .controls {
+    position: absolute;
+    top: 1rem;
+    right: 1rem;
   }
 `;
 
@@ -32,31 +59,29 @@ const DonorListItem = ({ donor, handleUpdate, handleDelete }) => {
     handleUpdate(donor);
   };
   return (
-    <DonorListItemContainer>
-      <div className="list-item" data-contact-stale={isStale(donor.last_contact)}>
-        <div className="donor-name">{donor.name}</div>
-        <div className="donor-contributions">
-          {/* <span className="label">Lifetime contribution</span> */}
-          {formatContribution(donor.total_donations)}
-        </div>
-        <div className="donor-contact">
-          <MailTo email={donor.email}>{donor.email}</MailTo>
-        </div>
-        <div className="donor-last-contact">{getDate(donor.last_contact)}</div>
-
-        <div className="donor-controls">
-          <ActionButton
-            imgSrc="/icons/clock.svg"
-            alt="Mark contacted"
-            onClick={() => update(donor)}
-          />
-          <ActionButton imgSrc="/icons/money-sign.svg" alt="Add donation" onClick={null} />
-          <ActionButton
-            imgSrc="/icons/trash.svg"
-            alt="Delete donor"
-            onClick={() => handleDelete(donor.id)}
-          />
-        </div>
+    <DonorListItemContainer data-contact-stale={isStale(donor.last_contact)}>
+      <div className="name">{donor.name}</div>
+      <div className="contributions">
+        {formatContribution(donor.total_donations)}
+        <div className="label">Lifetime contribution</div>
+      </div>
+      <div className="contact">
+        Last contacted {getDate(donor.last_contact)} <br />
+        <MailTo email={donor.email}>{donor.email}</MailTo>
+      </div>
+      {isStale(donor.last_contact) && <div className="stale">over 60 days</div>}
+      <div className="controls">
+        <ActionButton
+          imgSrc="/icons/clock.svg"
+          alt="Mark contacted"
+          onClick={() => update(donor)}
+        />
+        <ActionButton imgSrc="/icons/money-sign.svg" alt="Add donation" onClick={null} />
+        <ActionButton
+          imgSrc="/icons/trash.svg"
+          alt="Delete donor"
+          onClick={() => handleDelete(donor.id)}
+        />
       </div>
     </DonorListItemContainer>
   );
