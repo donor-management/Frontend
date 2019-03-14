c50735b6d6f143ec5c9de37817fe3d29026ea7db
Joel Bartlett <joel@murbar.com>
2019-03-14 11:02:59 -0400

    Tidy up

--- src/store/AppDataContext.js
+++ src/store/AppDataContext.js
@@ -1,26 +1,12 @@
 import React, { useEffect } from 'react';
 import useDonors from '../hooks/useDonors';
 import useCampaigns from '../hooks/useCampaigns';
-// import useDonationService from '../hooks/useDonationService';
 
 const AppDataContext = React.createContext();
 
 const AppDataProvider = ({ children }) => {
   const donorStore = useDonors();
   const campaignStore = useCampaigns();
-  // const [donations, donationActions] = useDonationService();
-  // to handle errors in the future
-  // const [appStatus, setAppStatus] = useState({
-  //   isLoading: false,
-  //   error: null
-  // });
-
-  // const toggleLoading = () => {
-  //   setAppStatus(prev => ({
-  //     ...prev,
-  //     isLoading: !prev.isLoading
-  //   }));
-  // };
 
   // logging
   useEffect(() => {
