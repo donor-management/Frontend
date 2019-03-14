6b48b104c0ae02ca7584b9495bb73d844f5cdf14
Joel Bartlett <joel@murbar.com>
2019-03-14 00:01:30 -0400

    Add stubs for future changes

--- src/store/AppDataContext.js
+++ src/store/AppDataContext.js
@@ -1,24 +1,37 @@
-import React, { useEffect, useState } from 'react';
-import campaignService from '../services/campaignService';
+import React, { useEffect } from 'react';
 import useDonorService from '../hooks/useDonorService';
 import useCampaignService from '../hooks/useCampaignService';
+// import useDonationService from '../hooks/useDonationService';
 
 const AppDataContext = React.createContext();
 
 const AppDataProvider = ({ children }) => {
   const [donors, donorActions] = useDonorService();
-  const [campaigns, campaignActions] = useCampaignService([]);
-  const [appStatus, setAppStatus] = useState({
-    isLoading: false,
-    error: null
-  });
+  const [campaigns, campaignActions] = useCampaignService();
+  // const [donations, donationActions] = useDonationService();
+  // to handle errors in the future
+  // const [appStatus, setAppStatus] = useState({
+  //   isLoading: false,
+  //   error: null
+  // });
 
-  const toggleLoading = () => {
-    setAppStatus(prev => ({
-      ...prev,
-      isLoading: !prev.isLoading
-    }));
-  };
+  // const toggleLoading = () => {
+  //   setAppStatus(prev => ({
+  //     ...prev,
+  //     isLoading: !prev.isLoading
+  //   }));
+  // };
+
+  // logging
+  useEffect(() => {
+    console.log('donors :', donors);
+  }, [donors]);
+  useEffect(() => {
+    console.log('campaigns :', campaigns);
+  }, [campaigns]);
+  // useEffect(() => {
+  //   console.log('donations :', donations);
+  // }, [donations]);
 
   return (
     <AppDataContext.Provider value={{ donors, donorActions, campaigns, campaignActions }}>
