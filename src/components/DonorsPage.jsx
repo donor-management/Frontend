471edc51132ccfcf7cb89998fafc9af25836be09
Joel Bartlett <joel@murbar.com>
2019-03-14 00:02:33 -0400

    Remove unused import

--- src/components/DonorsPage.jsx
+++ src/components/DonorsPage.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext } from 'react';
 import { AppDataContext } from '../store/AppDataContext';
 import DashNav from './DashNav';
 import DonorForm from './DonorForm';
