c6c28eebd0dbe0662a5884d942da5d8733dad66e
Joel Bartlett <joel@murbar.com>
2019-03-13 20:14:31 -0400

    Add styles back to file

--- src/components/CampaignsPage.jsx
+++ src/components/CampaignsPage.jsx
@@ -1,12 +1,68 @@
 import React, { useContext } from 'react';
+import styled from 'styled-components';
 import DashNav from './DashNav';
 import { AppDataContext } from '../store/AppDataContext';
-import DataListViewContainer from './common/DataListViewContainer';
-
 import Button from './common/Button';
 import useToggle from '../hooks/useToggle';
 import CampaignForm from './CampaignForm';
 import formatDollars from '../helpers/formatDollars';
+
+const CampaignPageContainer = styled.section`
+  /* .contacted span {
+    font-size: 80%;
+    color: crimson;
+    font-weight: normal;
+    padding-left: 1rem;
+  } */
+  font-size: 90%;
+  [data-contact-stale='true'] {
+    color: crimson;
+  }
+  .control {
+    /* position: relative; */
+    opacity: 0.5;
+    margin: 0 0.25rem;
+    padding: 0;
+    background: transparent;
+    vertical-align: baseline;
+    /* line-height: 1.5rem; */
+    img {
+      height: 1.5rem;
+      margin-bottom: -0.2rem;
+    }
+  }
+  .control:hover {
+    opacity: 1;
+  }
+  .list-item {
+    background: #f4f4f4;
+    margin-bottom: 0.5rem;
+    padding: 0.75rem;
+    border-radius: 0.5rem;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+  }
+  .list-item:hover {
+    background: #eee;
+  }
+  .donor-name {
+    width: 25%;
+  }
+  .donor-contributions {
+    width: 15%;
+  }
+  .donor-contact {
+    width: 25%;
+    overflow: hidden;
+  }
+  .donor-last-contact {
+    width: 20%;
+  }
+  .controls {
+    /* width: %; */
+  }
+`;
 
 const CampaignsPage = () => {
   const { campaigns, campaignActions } = useContext(AppDataContext);
@@ -49,7 +105,7 @@ const CampaignsPage = () => {
   return (
     <>
       <DashNav />
-      <DataListViewContainer>
+      <CampaignPageContainer>
         <h1>
           {pageTitle}{' '}
           {!showForm && (
@@ -60,7 +116,7 @@ const CampaignsPage = () => {
         </h1>
         {showForm && <CampaignForm toggle={toggleShowForm} />}
         {renderCampaigns()}
-      </DataListViewContainer>
+      </CampaignPageContainer>
     </>
   );
 };
