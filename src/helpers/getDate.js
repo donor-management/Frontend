dd54a728d13d3f21d2dd8fa4815b6c2ffab6300e
Joel Bartlett <joel@murbar.com>
2019-03-15 00:50:58 -0400

    Fix bug that showed stale tag for newly added donors

--- src/helpers/getDate.js
+++ src/helpers/getDate.js
@@ -2,5 +2,5 @@ import moment from 'moment';
 
 export default function getDate(timestamp) {
   const date = moment(timestamp).fromNow();
-  return date.includes('Invalid') ? 'Never' : date;
+  return timestamp ? date : null;
 }
